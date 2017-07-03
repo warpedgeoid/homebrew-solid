a3d9c36779f8a183e3a77f20b328aff514fcfe29
Rob Howard <rob@little-frank.intra.ecu.edu>
2017-07-03 12:27:55 -0400

    Restructure repository to fit typical tap structure.

---
+++ Formula/cgal@4.10.rb
@@ -0,0 +1,85 @@
+class CgalAT410 < Formula
+  desc "Computational Geometry Algorithm Library"
+  homepage "https://www.cgal.org/"
+  url "https://github.com/CGAL/cgal/releases/download/releases/CGAL-4.10/CGAL-4.10.tar.xz"
+  sha256 "56557da971b5310c2678ffc5def4109266666ff3adc7babbe446797ee2b90cca"
+
+  option :cxx11
+  option "with-qt", "Build ImageIO and Qt components of CGAL"
+  option "with-eigen", "Build with Eigen3 support"
+  option "with-lapack", "Build with LAPACK support"
+
+  deprecated_option "imaging" => "with-qt"
+  deprecated_option "with-imaging" => "with-qt"
+  deprecated_option "with-eigen3" => "with-eigen"
+  deprecated_option "with-qt5" => "with-qt"
+
+  depends_on "cmake" => :build
+  depends_on "mpfr"
+
+  depends_on "qt" => :optional
+  depends_on "eigen" => :optional
+
+  if build.cxx11?
+    depends_on "boost" => "c++11"
+    depends_on "gmp"   => "c++11"
+  else
+    depends_on "boost"
+    depends_on "gmp"
+  end
+
+  def install
+    ENV.cxx11 if build.cxx11?
+
+    args = std_cmake_args + %W[
+      -DCMAKE_BUILD_WITH_INSTALL_RPATH=ON
+      -DCMAKE_INSTALL_NAME_DIR=#{HOMEBREW_PREFIX}/lib
+    ]
+
+    if build.without? "qt"
+      args << "-DWITH_CGAL_Qt5=OFF" << "-DWITH_CGAL_ImageIO=OFF"
+    else
+      args << "-DWITH_CGAL_Qt5=ON" << "-DWITH_CGAL_ImageIO=ON"
+    end
+
+    if build.with? "eigen"
+      args << "-DWITH_Eigen3=ON"
+    else
+      args << "-DWITH_Eigen3=OFF"
+    end
+
+    if build.with? "lapack"
+      args << "-DWITH_LAPACK=ON"
+    else
+      args << "-DWITH_LAPACK=OFF"
+    end
+
+    system "cmake", ".", *args
+    system "make", "install"
+  end
+
+  test do
+    # https://doc.cgal.org/latest/Algebraic_foundations/Algebraic_foundations_2interoperable_8cpp-example.html
+    (testpath/"surprise.cpp").write <<-EOS.undent
+      #include <CGAL/basic.h>
+      #include <CGAL/Coercion_traits.h>
+      #include <CGAL/IO/io.h>
+      template <typename A, typename B>
+      typename CGAL::Coercion_traits<A,B>::Type
+      binary_func(const A& a , const B& b){
+          typedef CGAL::Coercion_traits<A,B> CT;
+          CGAL_static_assertion((CT::Are_explicit_interoperable::value));
+          typename CT::Cast cast;
+          return cast(a)*cast(b);
+      }
+      int main(){
+          std::cout<< binary_func(double(3), int(5)) << std::endl;
+          std::cout<< binary_func(int(3), double(5)) << std::endl;
+          return 0;
+      }
+    EOS
+    system ENV.cxx, "-I#{include}", "-L#{lib}", "-lCGAL",
+                    "surprise.cpp", "-o", "test"
+    assert_equal "15\n15", shell_output("./test").chomp
+  end
+end
---
+++ Formula/pymesh.rb
@@ -0,0 +1,51 @@
+# Documentation: http://docs.brew.sh/Formula-Cookbook.html
+#                http://www.rubydoc.info/github/Homebrew/brew/master/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+
+class Pymesh < Formula
+  desc "PyMesh is a rapid prototyping platform focused on geometry processing."
+  homepage "http://pymesh.readthedocs.org/en/latest/"
+  url "https://github.com/qnzhou/PyMesh.git"
+  version "0.10"
+  sha256 ""
+
+  depends_on "cmake" => :build
+  depends_on "swig" => :build
+  depends_on "boost"
+  depends_on "eigen"
+  depends_on :python3 => ["numpy", "scipy"]
+  depends_on "google-sparsehash" :recommended
+  depends_on "cgal" => ["4.10", :recommended]
+  depends_on "tetgen" :recommended
+  depends_on "libigl" :recommended
+  depends_on "cork" :recommended
+  depends_on "triangle" :recommended
+  depends_on "qhull" :recommended
+  depends_on "libclipper" :recommended
+  depends_on "carve" :recommended
+
+  def install
+    # ENV.deparallelize  # if your formula fails when building in parallel
+
+    # Remove unrecognized options if warned by configure
+    system "./configure", "--disable-debug",
+                          "--disable-dependency-tracking",
+                          "--disable-silent-rules",
+                          "--prefix=#{prefix}"
+    # system "cmake", ".", *std_cmake_args
+    system "make", "install" # if this fails, try separate make/make install steps
+  end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! For Homebrew/homebrew-core
+    # this will need to be a test that verifies the functionality of the
+    # software. Run the test with `brew test pymesh`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system "#{bin}/program", "do", "something"`.
+    system "false"
+  end
+end
